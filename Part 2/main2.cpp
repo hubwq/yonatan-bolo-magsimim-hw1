bb954e27c3b1fab67e5fb24f0c442238a213997f
unknown <hubwq215471160@gmail.com>
2024-11-13 18:37:23 +0200

    main

--- Part 2/main2.cpp
+++ Part 2/main2.cpp
@@ -1,14 +1,13 @@
-<<<<<<< HEAD
 #include "LinkedList.h"
 #include "Stack.h"
+#include "Utils.h"
 #include <iostream>
+#include <string>
 
 ////check LinkedList
-=======
 //#include "LinkedList.h"
 //#include <iostream>
 //
->>>>>>> 74b327bde64ecfeaa9b9df5a30f7df720ef9ab27
 //int main()
 //{
 //	Node* head = new Node;
@@ -34,32 +33,49 @@
 //	removeNode(&head);
 //
 //	return 0;
-<<<<<<< HEAD
 //}
 
-int main()
-{
-	Stack* head = new Stack;
-
-	initStack(head);
-
-	push(head, 4);
-
-	printStack(head);
-
-	push(head, 65);
-	push(head, 86);
-
-	printStack(head);
-
-	pop(head);
-
-	printStack(head);
-
-	cleanStack(head);
-
-	return 0;
-}
-=======
+////check Stack
+//int main()
+//{
+//	Stack* head = new Stack;
+//
+//	initStack(head);
+//
+//	push(head, 4);
+//
+//	printStack(head);
+//
+//	push(head, 65);
+//	push(head, 86);
+//
+//	printStack(head);
+//
+//	pop(head);
+//
+//	printStack(head);
+//
+//	cleanStack(head);
+//
+//	return 0;
 //}
->>>>>>> 74b327bde64ecfeaa9b9df5a30f7df720ef9ab27
+
+////check Utils
+//int main()
+//{
+//	int nums[] = {1, 2, 3, 4};
+//
+//	for (int i = 0; i < 4; i++)
+//	{
+//		std::cout << nums[i] << " ";
+//	}
+//	std::cout << std::endl;
+//
+//	reverse(nums, 4);
+//
+//	for (int i = 0; i < 4; i++)
+//	{
+//		std::cout << nums[i] << " ";
+//	}
+//	std::cout << std::endl;
+//}
