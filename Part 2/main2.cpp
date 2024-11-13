6e0b6ca67ea366b775027324b14663b5a7b4e7de
unknown <hubwq215471160@gmail.com>
2024-11-13 17:43:14 +0200

    Stack

--- Part 2/main2.cpp
+++ Part 2/main2.cpp
@@ -1,6 +1,8 @@
-//#include "LinkedList.h"
-//#include <iostream>
-//
+#include "LinkedList.h"
+#include "Stack.h"
+#include <iostream>
+
+////check LinkedList
 //int main()
 //{
 //	Node* head = new Node;
@@ -27,3 +29,27 @@
 //
 //	return 0;
 //}
+
+int main()
+{
+	Stack* head = new Stack;
+
+	initStack(head);
+
+	push(head, 4);
+
+	printStack(head);
+
+	push(head, 65);
+	push(head, 86);
+
+	printStack(head);
+
+	pop(head);
+
+	printStack(head);
+
+	cleanStack(head);
+
+	return 0;
+}
