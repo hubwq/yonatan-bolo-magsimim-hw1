74b327bde64ecfeaa9b9df5a30f7df720ef9ab27
unknown <hubwq215471160@gmail.com>
2024-11-13 16:20:04 +0200

    part 2

---
+++ Part 2/main2.cpp
@@ -0,0 +1,29 @@
+//#include "LinkedList.h"
+//#include <iostream>
+//
+//int main()
+//{
+//	Node* head = new Node;
+//	Node* newNode = new Node;
+//
+//	initNode(head);
+//	initNode(newNode);
+//
+//	setNode(&head, 7);
+//	setNode(&newNode, 3);
+//
+//	printList(head);
+//	printList(newNode);
+//
+//	addNode(&head, newNode);
+//
+//	printList(head);
+//
+//	removeNode(&head);
+//
+//	printList(head);
+//
+//	removeNode(&head);
+//
+//	return 0;
+//}
